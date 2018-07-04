2f3f9cad0c404865d726d35040a3a8689111681d
Duong Do <dohoangduong95@gmail.com>
2018-07-04 16:30:45 -0400

    Fix check_pythagorean func

--- myproj/math.py
+++ myproj/math.py
@@ -6,4 +6,4 @@ def check_pythagorean(a, b, c):
     :return: Check if the number a, b, c satisfy Pythagorean theorem
     a^2 + b^2 = c^2, a^2 + c^2 = b^2, a^2 = b^2 + c^2
     """
-    return a**2 + b**2 == c**2
+    return a**2 + b**2 == c**2 or a**2 + c**2 == b**2 or a**2 == b**2 + c**2
