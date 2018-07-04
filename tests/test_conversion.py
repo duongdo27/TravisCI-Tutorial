5c0cbeae5a298bc1af9e0cf98988b5f684374545
Duong Do <dohoangduong95@gmail.com>
2018-07-04 16:19:37 -0400

    Add test for convert_fahrenheit_to_celsius

--- tests/test_conversion.py
+++ tests/test_conversion.py
@@ -1,4 +1,5 @@
 from myproj.conversion import convert_celsius_to_fahrenheit
+from myproj.conversion import convert_fahrenheit_to_celsius
 
 
 def test_celsius_to_fahrenheit():
@@ -8,3 +9,12 @@ def test_celsius_to_fahrenheit():
 
 def test_celsius_to_fahrenheit_none():
     assert convert_celsius_to_fahrenheit(None) is None
+
+
+def test_fahrenheit_to_celsius():
+    assert convert_fahrenheit_to_celsius(32) == 0
+    assert convert_fahrenheit_to_celsius(212) == 100
+
+
+def test_fahrenheit_to_celsius_none():
+    assert convert_fahrenheit_to_celsius(None) is None
